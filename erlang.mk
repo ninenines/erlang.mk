cbc68b8e5d50f5bdf8bf1031e68627994819457d
Loïc Hoguin <essen@ninenines.eu>
2013-08-22 19:37:47 +0200

    Use a proper file extension for the packages file
    We'll keep the .txt file for a month or so before deleting it.

--- erlang.mk
+++ erlang.mk
@@ -21,7 +21,7 @@ PROJECT ?= $(notdir $(CURDIR))
 PKG_FILE ?= $(CURDIR)/.erlang.mk.packages.v1
 export PKG_FILE
 
-PKG_FILE_URL ?= https://raw.github.com/extend/erlang.mk/master/packages.v1.txt
+PKG_FILE_URL ?= https://raw.github.com/extend/erlang.mk/master/packages.v1.tsv
 
 define get_pkg_file
 	wget -O $(PKG_FILE) $(PKG_FILE_URL)
