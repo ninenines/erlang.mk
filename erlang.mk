807b1d9fc4a8d53b2120c350fd27fce394bb66cb
Daniel White <daniel@whitehouse.id.au>
2013-08-24 22:45:46 +1000

    Set ERL_LIBS to $(DEPS_DIR) rather than $(DEPS)
    This fixes a problem introduced by commit 1b6aa025 such that the
    dependencies were no longer being included in compilation.
    
    Noticed due to modules in a project being unable to find behaviours
    defined in dependencies.

--- erlang.mk
+++ erlang.mk
@@ -62,7 +62,7 @@ ALL_TEST_DEPS_DIRS = $(addprefix $(DEPS_DIR)/,$(TEST_DEPS))
 
 # Application.
 
-ERL_LIBS ?= $(DEPS)
+ERL_LIBS ?= $(DEPS_DIR)
 export ERL_LIBS
 
 ERLC_OPTS ?= -Werror +debug_info +warn_export_all +warn_export_vars \

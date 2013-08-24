ninenines/erlang.mk

commit 1b6aa025514d131082ca10fca8c791af1bddaf8d

Allow higher level makefile to specify ERL_LIBS

erlang.mk
@@ -60,6 +60,9 @@ ALL_TEST_DEPS_DIRS = $(addprefix $(DEPS_DIR)/,$(TEST_DEPS))
 
 # Application.
 
+ERL_LIBS ?= $(DEPS)
+export ERL_LIBS
+
 ERLC_OPTS ?= -Werror +debug_info +warn_export_all +warn_export_vars \
 	+warn_shadow_vars +warn_obsolete_guard # +bin_opt_info +warn_missing_spec
 COMPILE_FIRST ?=
@@ -78,7 +81,7 @@ app: ebin/$(PROJECT).app
 		> ebin/$(PROJECT).app
 
 define compile_erl
-	$(erlc_verbose) ERL_LIBS=$(DEPS_DIR) erlc -v $(ERLC_OPTS) -o ebin/ \
+	$(erlc_verbose) erlc -v $(ERLC_OPTS) -o ebin/ \
 		-pa ebin/ -I include/ $(COMPILE_FIRST_PATHS) $(1)
 endef
 
@@ -164,7 +167,7 @@ build-test-deps: $(ALL_TEST_DEPS_DIRS)
 	@for dep in $(ALL_TEST_DEPS_DIRS) ; do $(MAKE) -C $$dep; done
 
 build-tests: build-test-deps
-	$(gen_verbose) ERL_LIBS=$(DEPS_DIR) erlc -v $(ERLC_OPTS) -o test/ \
+	$(gen_verbose) erlc -v $(ERLC_OPTS) -o test/ \
 		$(wildcard test/*.erl test/*/*.erl) -pa ebin/
 
 CT_RUN = ct_run \

commit 807b1d9fc4a8d53b2120c350fd27fce394bb66cb

Set ERL_LIBS to $(DEPS_DIR) rather than $(DEPS)
This fixes a problem introduced by commit 1b6aa025 such that the
dependencies were no longer being included in compilation.

Noticed due to modules in a project being unable to find behaviours
defined in dependencies.

erlang.mk
@@ -62,7 +62,7 @@ ALL_TEST_DEPS_DIRS = $(addprefix $(DEPS_DIR)/,$(TEST_DEPS))
 
 # Application.
 
-ERL_LIBS ?= $(DEPS)
+ERL_LIBS ?= $(DEPS_DIR)
 export ERL_LIBS
 
 ERLC_OPTS ?= -Werror +debug_info +warn_export_all +warn_export_vars \