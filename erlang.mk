dcce360c8f1df2086ab2376f1d7dd2834760485e
Sean Chalmers <sclhiannan@gmail.com>
2013-11-20 16:42:34 +0100

    Change assignment to std '='.
    Original commit was using the 'expanded variable' assignment operator
    that would have prevented the ```ERL_LIBS``` value from being updated if
    the ```DEP_DIRS``` value was changed later on during execution.
    
    TODO: Ensure this is a Non *nix compatible change as calling shell echo
    may have unwanted side effects.

--- erlang.mk
+++ erlang.mk
@@ -89,7 +89,7 @@ ALL_TEST_DEPS_DIRS = $(addprefix $(DEPS_DIR)/,$(TEST_DEPS))
 
 # Application.
 
-ERL_LIBS := $(shell echo $(ERL_LIBS)):$(DEPS_DIR)
+ERL_LIBS = $(shell echo $(ERL_LIBS)):$(DEPS_DIR)
 export ERL_LIBS
 
 ERLC_OPTS ?= -Werror +debug_info +warn_export_all +warn_export_vars \
