e20a489ac30507f42393cb9f7cfca760f76d7527
fsg <felixgallo@gmail.com>
2014-11-07 14:15:53 -0800

    add extra options to common test plugin

--- plugins/ct.mk
+++ plugins/ct.mk
@@ -8,8 +8,9 @@
 CT_OPTS ?=
 CT_SUITES ?=
 
+EXTRA_ERLC_OPTS ?=
 TEST_ERLC_OPTS ?= +debug_info +warn_export_vars +warn_shadow_vars +warn_obsolete_guard
-TEST_ERLC_OPTS += -DTEST=1 -DEXTRA=1 +'{parse_transform, eunit_autoexport}'
+TEST_ERLC_OPTS += -DTEST=1 -DEXTRA=1 +'{parse_transform, eunit_autoexport}' $(EXTRA_ERLC_OPTS)
 
 # Core targets.
 
