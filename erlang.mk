22a538441867ed7527803a34d3c6c81788bad925
Mark Nijhof <mark.nijhof@cre8ivethought.com>
2014-12-29 15:47:59 +0100

    Updated relx to version v1.1.0

--- erlang.mk
+++ erlang.mk
@@ -1088,7 +1088,7 @@ RELX_CONFIG ?= $(CURDIR)/relx.config
 RELX ?= $(CURDIR)/relx
 export RELX
 
-RELX_URL ?= https://github.com/erlware/relx/releases/download/v1.0.2/relx
+RELX_URL ?= https://github.com/erlware/relx/releases/download/v1.1.0/relx
 RELX_OPTS ?=
 RELX_OUTPUT_DIR ?= _rel
 
