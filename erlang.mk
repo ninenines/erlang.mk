25831f56d56f06c2cdde570039f9fde99427596d
Ilya Khaprov <dead.trickster@gmail.com>
2015-03-28 11:35:13 -0700

    elixir plugin

--- erlang.mk
+++ erlang.mk
@@ -1273,3 +1273,28 @@ triq: test-build
 	$(gen_verbose) $(call triq_run,[true] =:= lists:usort([triq:check(M) || M <- [$(MODULES)]]))
 endif
 endif
+
+# Copyright (c) 2015, Ilya Khaprov <ilya.khaprov@publitechs.com>
+# This file is part of erlang.mk and subject to the terms of the ISC License.
+
+# Configuration.
+
+ELIXIRC = elixirc
+ELIXIRC_OPTS ?= --warnings-as-errors
+ELIXIRC_OUT = -o ebin/
+
+# Core targets.
+
+define compile_elixir
+	ERL_COMPILER_OPTS="$(ERLC_OPTS)" $(ELIXIRC) $(ELIXIRC_OPTS) $(ELIXIRC_OUT) $(1)
+endef
+
+ifneq ($(wildcard src/),)
+ebin/$(PROJECT).app:: $(shell find src -type f -name \*.ex 2>/dev/null)
+	$(if $(strip $?),$(call compile_elixir,$?))
+endif
+
+ifneq ($(wildcard lib/),)
+ebin/$(PROJECT).app:: $(shell find lib -type f -name \*.ex 2>/dev/null)
+	$(if $(strip $?),$(call compile_elixir,$?))
+endif
