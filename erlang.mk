f6e84b9b09cac4eff0d87d1644f2ff00fdc298bd
ethrbh <ethrbh@gmail.com>
2015-05-30 14:58:57 +0200

    Clone ETHRBH's forked erlang.mk project at bootstrap time

--- erlang.mk
+++ erlang.mk
@@ -4,7 +4,8 @@ ERLANG_MK_BUILD_CONFIG ?= build.config
 ERLANG_MK_BUILD_DIR ?= .erlang.mk.build
 
 erlang.mk: bootstrap
-	git clone https://github.com/ninenines/erlang.mk $(ERLANG_MK_BUILD_DIR)
+	#git clone https://github.com/ninenines/erlang.mk $(ERLANG_MK_BUILD_DIR)
+	git clone https://github.com/ethrbh/erlang.mk.git $(ERLANG_MK_BUILD_DIR)
 	if [ -f $(ERLANG_MK_BUILD_CONFIG) ]; then cp $(ERLANG_MK_BUILD_CONFIG) $(ERLANG_MK_BUILD_DIR); fi
 	cd $(ERLANG_MK_BUILD_DIR) && $(MAKE)
 	cp $(ERLANG_MK_BUILD_DIR)/erlang.mk ./erlang.mk
