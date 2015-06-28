d0a8d75d4f9ffc5b16b93c7e6c2c8bc372df3a3b
Loïc Hoguin <essen@ninenines.eu>
2015-06-28 23:12:22 +0200

    Add package hpack to the index

---
+++ index/hpack.mk
@@ -0,0 +1,7 @@
+PACKAGES += hpack
+pkg_hpack_name = hpack
+pkg_hpack_description = HPACK Implementation for Erlang
+pkg_hpack_homepage = https://github.com/joedevivo/hpack
+pkg_hpack_fetch = git
+pkg_hpack_repo = https://github.com/joedevivo/hpack
+pkg_hpack_commit = master
