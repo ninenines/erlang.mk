dd7ddb509163dd28ee1afc15a393fba2493656be
Loïc Hoguin <essen@ninenines.eu>
2015-07-03 09:47:15 +0200

    Package barrel was renamed to barrel_tcp overnight

---
+++ index/barrel_tcp.mk
@@ -0,0 +1,7 @@
+PACKAGES += barrel_tcp
+pkg_barrel_tcp_name = barrel_tcp
+pkg_barrel_tcp_description = barrel is a generic TCP acceptor pool with low latency in Erlang.
+pkg_barrel_tcp_homepage = https://github.com/benoitc-attic/barrel_tcp
+pkg_barrel_tcp_fetch = git
+pkg_barrel_tcp_repo = https://github.com/benoitc-attic/barrel_tcp
+pkg_barrel_tcp_commit = master
