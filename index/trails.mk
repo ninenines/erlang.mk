eac26c77b7f7a1bcefd8f50987a64881f53d50fd
Loïc Hoguin <essen@ninenines.eu>
2015-07-04 12:51:23 +0200

    Add package trails to the index

---
+++ index/trails.mk
@@ -0,0 +1,7 @@
+PACKAGES += trails
+pkg_trails_name = trails
+pkg_trails_description = A couple of improvements over Cowboy Routes
+pkg_trails_homepage = http://inaka.github.io/cowboy-trails/
+pkg_trails_fetch = git
+pkg_trails_repo = https://github.com/inaka/cowboy-trails
+pkg_trails_commit = master
