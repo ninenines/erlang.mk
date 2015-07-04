ea7424b277c7a4543f1b08d0e227782f4bae079f
Loïc Hoguin <essen@ninenines.eu>
2015-07-04 12:53:25 +0200

    Add package gut to the index

---
+++ index/gut.mk
@@ -0,0 +1,7 @@
+PACKAGES += gut
+pkg_gut_name = gut
+pkg_gut_description = gut is a template printing, aka scaffolding, tool for Erlang. Like rails generate or yeoman
+pkg_gut_homepage = https://github.com/unbalancedparentheses/gut
+pkg_gut_fetch = git
+pkg_gut_repo = https://github.com/unbalancedparentheses/gut
+pkg_gut_commit = master
