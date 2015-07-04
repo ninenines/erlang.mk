c2debce2bfcf2400905ff56c4a80e4c3251c6e56
Loïc Hoguin <essen@ninenines.eu>
2015-07-04 12:56:55 +0200

    Add package prop to the index

---
+++ index/prop.mk
@@ -0,0 +1,7 @@
+PACKAGES += prop
+pkg_prop_name = prop
+pkg_prop_description = An Erlang code scaffolding and generator system.
+pkg_prop_homepage = https://github.com/nuex/prop
+pkg_prop_fetch = git
+pkg_prop_repo = https://github.com/nuex/prop
+pkg_prop_commit = master
