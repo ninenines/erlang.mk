56cbd9e0919b884025d9b21c4ad3de8bcc6fe573
Loïc Hoguin <essen@ninenines.eu>
2015-07-06 23:40:21 +0200

    Add package syn to the index

---
+++ index/syn.mk
@@ -0,0 +1,7 @@
+PACKAGES += syn
+pkg_syn_name = syn
+pkg_syn_description = A global process registry for Erlang.
+pkg_syn_homepage = https://github.com/ostinelli/syn
+pkg_syn_fetch = git
+pkg_syn_repo = https://github.com/ostinelli/syn
+pkg_syn_commit = master
