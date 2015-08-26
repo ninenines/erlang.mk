d86f65346df71bd5608315fc244ed49cbf7aa431
Loïc Hoguin <essen@ninenines.eu>
2015-08-26 16:09:41 +0200

    Add package riak_control to the index

---
+++ index/riak_control.mk
@@ -0,0 +1,7 @@
+PACKAGES += riak_control
+pkg_riak_control_name = riak_control
+pkg_riak_control_description = Webmachine-based administration interface for Riak.
+pkg_riak_control_homepage = https://github.com/basho/riak_control
+pkg_riak_control_fetch = git
+pkg_riak_control_repo = https://github.com/basho/riak_control
+pkg_riak_control_commit = master
