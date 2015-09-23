2ef86140a557453a420672ef52b3863ce98f7c49
Loïc Hoguin <essen@ninenines.eu>
2015-09-24 00:32:32 +0200

    Add test for the hex fetch method

--- test/core_deps.mk
+++ test/core_deps.mk
@@ -1,6 +1,6 @@
 # Core: Packages and dependencies.
 
-CORE_DEPS_CASES = build-c-8cc build-c-imagejs build-erl build-js dep-commit doc fetch-cp fetch-git fetch-hg fetch-svn otp pkg rel search shell test
+CORE_DEPS_CASES = build-c-8cc build-c-imagejs build-erl build-js dep-commit doc fetch-cp fetch-git fetch-hex fetch-hg fetch-svn otp pkg rel search shell test
 CORE_DEPS_TARGETS = $(addprefix core-deps-,$(CORE_DEPS_CASES))
 CORE_DEPS_CLEAN_TARGETS = $(addprefix clean-,$(CORE_DEPS_TARGETS))
 
@@ -248,6 +248,41 @@ core-deps-fetch-git: build clean-core-deps-fetch-git
 	$t sed -i.bak '2i\
 DEPS = cowboy\
 dep_cowboy = git https://github.com/ninenines/cowboy 1.0.0\
+' $(APP)/Makefile
+
+ifdef LEGACY
+	$i "Add Cowboy to the applications key in the .app.src file"
+	$t sed -i.bak '8i\
+			cowboy,' $(APP)/src/$(APP).app.src
+endif
+
+	$i "Build the application"
+	$t $(MAKE) -C $(APP) $v
+
+	$i "Check that all dependencies were fetched"
+	$t test -d $(APP)/deps/cowboy
+	$t test -d $(APP)/deps/cowlib
+	$t test -d $(APP)/deps/ranch
+
+	$i "Check that the application was compiled correctly"
+	$t $(ERL) -pa $(APP)/ebin/ $(APP)/deps/*/ebin/ -eval " \
+		[ok = application:load(App) || App <- [$(APP), cowboy, cowlib, ranch]], \
+		{ok, Deps} = application:get_key($(APP), applications), \
+		true = lists:member(cowboy, Deps), \
+		{ok, \"1.0.0\"} = application:get_key(cowboy, vsn), \
+		halt()"
+
+core-deps-fetch-hex: build clean-core-deps-fetch-hex
+
+	$i "Bootstrap a new OTP library named $(APP)"
+	$t mkdir $(APP)/
+	$t cp ../erlang.mk $(APP)/
+	$t $(MAKE) -C $(APP) -f erlang.mk bootstrap-lib $v
+
+	$i "Add Cowboy 1.0.0 to the list of dependencies"
+	$t sed -i.bak '2i\
+DEPS = cowboy\
+dep_cowboy = hex 1.0.0\
 ' $(APP)/Makefile
 
 ifdef LEGACY
