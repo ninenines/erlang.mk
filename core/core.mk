003ed2490a9c1212ab1de245822c7d263a21aedb
Loïc Hoguin <essen@ninenines.eu>
2015-09-27 21:55:53 +0200

    Fix core_find on stock OSX
    OSX find has this terrible behavior:
    
    $ find test/core_app_asn1/src/ -type f -name \*
    test/core_app_asn1/src//CAP.erl
    test/core_app_asn1/src//Def.erl
    test/core_app_asn1/src//use_cap.erl
    test/core_app_asn1/src//use_def.erl
    
    It's been long fixed everywhere else. Change the core_find function
    to remove the trailing / if any before calling find.

--- core/core.mk
+++ core/core.mk
@@ -174,7 +174,7 @@ endif
 
 core_eq = $(and $(findstring $(1),$(2)),$(findstring $(2),$(1)))
 
-core_find = $(if $(wildcard $1),$(shell find $1 -type f -name $(subst *,\*,$2)))
+core_find = $(if $(wildcard $1),$(shell find $(1:%/=%) -type f -name $(subst *,\*,$2)))
 
 core_lc = $(subst A,a,$(subst B,b,$(subst C,c,$(subst D,d,$(subst E,e,$(subst F,f,$(subst G,g,$(subst H,h,$(subst I,i,$(subst J,j,$(subst K,k,$(subst L,l,$(subst M,m,$(subst N,n,$(subst O,o,$(subst P,p,$(subst Q,q,$(subst R,r,$(subst S,s,$(subst T,t,$(subst U,u,$(subst V,v,$(subst W,w,$(subst X,x,$(subst Y,y,$(subst Z,z,$(1)))))))))))))))))))))))))))
 
