ea973ce662f8d4216c9610679c566b6b332918da
bullno1 <thebullno1@gmail.com>
2015-10-04 22:47:09 +0800

    Add package reload_mk to the index

---
+++ index/reload_mk.mk
@@ -0,0 +1,7 @@
+PACKAGES += reload_mk
+pkg_reload_mk_name = reload_mk
+pkg_reload_mk_description = Live reload plugin for erlang.mk.
+pkg_reload_mk_homepage = https://github.com/bullno1/reload.mk
+pkg_reload_mk_fetch = git
+pkg_reload_mk_repo = https://github.com/bullno1/reload.mk
+pkg_reload_mk_commit = master
