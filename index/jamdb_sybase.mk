9e79cb0972df94d63fa6e5284b0465487937ec26
Sergiy Kostyushkin <s.kostyushkin@gmail.com>
2015-10-20 12:07:21 +0300

    Add package jamdb_sybase to the index

---
+++ index/jamdb_sybase.mk
@@ -0,0 +1,7 @@
+PACKAGES += jamdb_sybase
+pkg_jamdb_sybase_name = jamdb_sybase
+pkg_jamdb_sybase_description = Erlang driver for SAP Sybase ASE
+pkg_jamdb_sybase_homepage = https://github.com/erlangbureau/jamdb_sybase
+pkg_jamdb_sybase_fetch = git
+pkg_jamdb_sybase_repo = https://github.com/erlangbureau/jamdb_sybase
+pkg_jamdb_sybase_commit = 0.6.0
