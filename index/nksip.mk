e79528f1725104d4bc2325498526dd8288b0b8d1
Loïc Hoguin <essen@ninenines.eu>
2015-11-22 00:35:36 +0100

    Add package nksip to the index

---
+++ index/nksip.mk
@@ -0,0 +1,7 @@
+PACKAGES += nksip
+pkg_nksip_name = nksip
+pkg_nksip_description = Erlang SIP application server
+pkg_nksip_homepage = https://github.com/kalta/nksip
+pkg_nksip_fetch = git
+pkg_nksip_repo = https://github.com/kalta/nksip
+pkg_nksip_commit = master
