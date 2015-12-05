6ba732c25f0f8bb702dd2c20da989f017ebcc07f
Loïc Hoguin <essen@ninenines.eu>
2015-12-05 11:44:50 +0100

    Temporarily remove restc and oauth2c
    The restc project has dependencies that require to be logged in
    to fetch them, and this breaks the packages test suite. The oauth2c
    project depends on restc.
    
    I sent a pull request at https://github.com/kivra/restclient/pull/8
    
    The packages can be added back just fine after this pull request
    get merged.
    
    In the future it might be worth having a value marking packages
    as broken so they are skipped by the test suite.

--- index/oauth2c.mk
+++
@@ -1,7 +0,0 @@
-PACKAGES += oauth2c
-pkg_oauth2c_name = oauth2c
-pkg_oauth2c_description = Erlang OAuth2 Client
-pkg_oauth2c_homepage = https://github.com/kivra/oauth2_client
-pkg_oauth2c_fetch = git
-pkg_oauth2c_repo = https://github.com/kivra/oauth2_client
-pkg_oauth2c_commit = master
