89dde87f45c228c04fbd1a9484ce9736637d9cd8
Petr Gotthard <petr.gotthard@centrum.cz>
2015-12-06 05:00:33 -0800

    Add package gen_coap to the index

---
+++ index/gen_coap.mk
@@ -0,0 +1,7 @@
+PACKAGES += gen_coap
+pkg_gen_coap_name = gen_coap
+pkg_gen_coap_description = Generic Erlang CoAP Client/Server
+pkg_gen_coap_homepage = https://github.com/gotthardp/gen_coap
+pkg_gen_coap_fetch = git
+pkg_gen_coap_repo = https://github.com/gotthardp/gen_coap
+pkg_gen_coap_commit = master
