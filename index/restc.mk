7cbb09937960b392ab619076a12e77ebbf88407a
Loïc Hoguin <essen@ninenines.eu>
2015-12-14 19:46:20 +0100

    Add restc back to the index

---
+++ index/restc.mk
@@ -0,0 +1,7 @@
+PACKAGES += restc
+pkg_restc_name = restc
+pkg_restc_description = Erlang Rest Client
+pkg_restc_homepage = https://github.com/kivra/restclient
+pkg_restc_fetch = git
+pkg_restc_repo = https://github.com/kivra/restclient
+pkg_restc_commit = master
