c87db152e2bebb9b2f18107ac83daf986961f361
benoitc <bchesneau@gmail.com>
2015-12-17 00:08:18 +0100

    add mimerl to the index

---
+++ index/mimerl.mk
@@ -0,0 +1,7 @@
+PACKAGES += mimerl
+pkg_mimerl_name = mimerl
+pkg_mimerl_description = library to handle mimetypes
+pkg_mimerl_homepage = https://github.com/benoitc/mimerl
+pkg_mimerl_fetch = git
+pkg_mimerl_repo = https://github.com/benoitc/mimerl
+pkg_mimerl_commit = master
