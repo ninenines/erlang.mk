3a0614508a88a9e30be2f47bc209e7d1e6f785b2
Loïc Hoguin <essen@ninenines.eu>
2015-12-17 12:17:58 +0100

    Add package i18n to the index

---
+++ index/i18n.mk
@@ -0,0 +1,7 @@
+PACKAGES += i18n
+pkg_i18n_name = i18n
+pkg_i18n_description = International components for unicode from Erlang (unicode, date, string, number, format, locale, localization, transliteration, icu4e)
+pkg_i18n_homepage = https://github.com/erlang-unicode/i18n
+pkg_i18n_fetch = git
+pkg_i18n_repo = https://github.com/erlang-unicode/i18n
+pkg_i18n_commit = master
