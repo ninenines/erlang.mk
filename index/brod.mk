802fcc4883742179f7bf004906df46587e7a5b16
Ivan Dyachkov <ivan.dyachkov@klarna.com>
2016-01-07 10:25:06 +0100

    Add package brod to the index

---
+++ index/brod.mk
@@ -0,0 +1,7 @@
+PACKAGES += brod
+pkg_brod_name = brod
+pkg_brod_description = Kafka client in Erlang
+pkg_brod_homepage = https://github.com/klarna/brod
+pkg_brod_fetch = git
+pkg_brod_repo = https://github.com/klarna/brod.git
+pkg_brod_commit = master
