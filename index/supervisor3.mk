e8af0ad13e59fadc87df60a84e8f72d087acad34
Ivan Dyachkov <ivan.dyachkov@klarna.com>
2016-01-07 10:22:10 +0100

    Add package supervisor3 to the index

---
+++ index/supervisor3.mk
@@ -0,0 +1,7 @@
+PACKAGES += supervisor3
+pkg_supervisor3_name = supervisor3
+pkg_supervisor3_description = OTP supervisor with additional strategies
+pkg_supervisor3_homepage = https://github.com/klarna/supervisor3
+pkg_supervisor3_fetch = git
+pkg_supervisor3_repo = https://github.com/klarna/supervisor3.git
+pkg_supervisor3_commit = master
