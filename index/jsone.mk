f748560cdfc0feb6d3fad1b11dd0da32ac4a09dd
antik486 <antik486@gmail.com>
2016-02-16 13:14:38 +0200

    add pkg https://github.com/sile/jsone

--- index/jsone.mk
+++ index/jsone.mk
@@ -1,7 +1,7 @@
 PACKAGES += jsone
 pkg_jsone_name = jsone
-pkg_jsone_description = 
-pkg_jsone_homepage = 
-pkg_jsone_fetch = 
-pkg_jsone_repo = 
-pkg_jsone_commit = 
+pkg_jsone_description =  An Erlang library for encoding, decoding JSON data.
+pkg_jsone_homepage = https://github.com/sile/jsone
+pkg_jsone_fetch = git
+pkg_jsone_repo = https://github.com/sile/jsone
+pkg_jsone_commit = master
