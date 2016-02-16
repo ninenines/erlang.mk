e2a546899094cc318ea0a14f5a93dd7ca39d270a
antik486 <antik486@gmail.com>
2016-02-16 13:34:07 +0200

    Add package jsone to the index

---
+++ index/jsone.mk
@@ -0,0 +1,7 @@
+PACKAGES += jsone
+pkg_jsone_name = jsone
+pkg_jsone_description = An Erlang library for encoding, decoding JSON data.
+pkg_jsone_homepage = https://github.com/sile/jsone.git
+pkg_jsone_fetch = git
+pkg_jsone_repo = https://github.com/sile/jsone.git
+pkg_jsone_commit = master
