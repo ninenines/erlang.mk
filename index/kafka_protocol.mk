4e5899fed96858bb0a6a32eebd38e37e560b7649
Zaiming Shi <zmstone@gmail.com>
2016-02-26 20:13:28 +0100

    add kafka_protocol

---
+++ index/kafka_protocol.mk
@@ -0,0 +1,7 @@
+PACKAGES += kafka_protocol
+pkg_kafka_protocol_name = kafka_protocol
+pkg_kafka_protocol_description = Kafka protocol Erlang library
+pkg_kafka_protocol_homepage = https://github.com/klarna/kafka_protocol
+pkg_kafka_protocol_fetch = git
+pkg_kafka_protocol_repo = https://github.com/klarna/kafka_protocol.git
+pkg_kafka_protocol_commit = master
