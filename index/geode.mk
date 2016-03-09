6420d8bdefff9fa2c3ee4aff47e1633ad36cc073
Aleksandar Radulovic <alex@a13x.net>
2016-03-09 10:18:17 +0100

    add geode project
    Adding geode - "Geohash / proximity searches in pure, uncut Erlang"

---
+++ index/geode.mk
@@ -0,0 +1,7 @@
+PACKAGES += geode
+pkg_geode_name = geode
+pkg_geode_description = geohash/proximity lookup in pure, uncut erlang.
+pkg_geode_homepage = https://github.com/bradfordw/geode
+pkg_geode_fetch = git
+pkg_geode_repo = https://github.com/bradfordw/geode
+pkg_geode_commit = master
