cba40ae35bd3d63eaffde194b48f380bfc91f379
Aleksandar Radulovic <alex@a13x.net>
2016-03-18 22:46:49 +0100

    Use the develop branch
    Basho uses the develop branch (not master) and that one is most up-to-date

--- index/bitcask.mk
+++ index/bitcask.mk
@@ -4,4 +4,4 @@ pkg_bitcask_description = because you need another a key/value storage engine
 pkg_bitcask_homepage = https://github.com/basho/bitcask
 pkg_bitcask_fetch = git
 pkg_bitcask_repo = https://github.com/basho/bitcask
-pkg_bitcask_commit = master
+pkg_bitcask_commit = develop
