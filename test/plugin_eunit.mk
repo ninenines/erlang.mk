81004c3de98bc07c29f33125e9ee37c89a0323c3
crownedgrouse <eric.pailleau@wanadoo.fr>
2016-04-05 00:41:59 +0200

    Add test for eunit accessing priv_dir

--- test/plugin_eunit.mk
+++ test/plugin_eunit.mk
@@ -1,6 +1,6 @@
 # EUnit plugin.
 
-EUNIT_CASES = all apps-only check erl-opts fun mod test-dir tests
+EUNIT_CASES = all apps-only check erl-opts fun mod test-dir tests privdir
 EUNIT_TARGETS = $(addprefix eunit-,$(EUNIT_CASES))
 
 .PHONY: eunit $(EUNIT_TARGETS)
@@ -207,4 +207,22 @@ eunit-tests: build clean
 		"-endif." > $(APP)/src/$(APP).erl
 
 	$i "Check that EUnit runs on 'make tests'"
+	$t $(MAKE) -C $(APP) tests | grep -q "Test passed."
+
+eunit-privdir: build clean
+
+	$i "Bootstrap a new OTP application named $(APP)"
+	$t mkdir $(APP)/
+	$t cp ../erlang.mk $(APP)/
+	$t $(MAKE) -C $(APP) -f erlang.mk bootstrap $v
+
+	$i "Generate a module containing EUnit tests"
+	$t printf "%s\n" \
+		"-module($(APP))." \
+		"-ifdef(TEST)." \
+		"-include_lib(\"eunit/include/eunit.hrl\")." \
+		"ok_test() -> ?assert(is_list(code:priv_dir($(APP))) =:= true)." \
+		"-endif." > $(APP)/src/$(APP).erl
+
+	$i "Check that EUnit access to project priv_dir"
 	$t $(MAKE) -C $(APP) tests | grep -q "Test passed."
