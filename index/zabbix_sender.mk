1b664efb5e99473d626f6117115726a463dd8991
antik486 <antik486@gmail.com>
2016-04-22 12:59:36 +0300

    Add package zabbix_sender to the index

---
+++ index/zabbix_sender.mk
@@ -0,0 +1,7 @@
+PACKAGES += zabbix_sender
+pkg_zabbix_sender_name = zabbix_sender
+pkg_zabbix_sender_description = Zabbix trapper for sending data to Zabbix in pure Erlang
+pkg_zabbix_sender_homepage = https://github.com/stalkermn/zabbix_sender
+pkg_zabbix_sender_fetch = git
+pkg_zabbix_sender_repo = https://github.com/stalkermn/zabbix_sender.git
+pkg_zabbix_sender_commit = master
