d2eb8b1cbf3a2d1882c475091f96ddc2ac8e8c00
Roberto Ostinelli <roberto@ostinelli.net>
2016-05-03 11:04:09 +0200

    Update syn.mk

--- index/syn.mk
+++ index/syn.mk
@@ -1,6 +1,6 @@
 PACKAGES += syn
 pkg_syn_name = syn
-pkg_syn_description = A global process registry for Erlang.
+pkg_syn_description = A global Process Registry and Process Group manager for Erlang.
 pkg_syn_homepage = https://github.com/ostinelli/syn
 pkg_syn_fetch = git
 pkg_syn_repo = https://github.com/ostinelli/syn
