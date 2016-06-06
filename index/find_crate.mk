4e12e6a3184d48c2032ba9b7561d14399d29331e
Daniel Goertzen <daniel.goertzen@gmail.com>
2016-06-06 15:14:46 -0500

    Add Rust plugin and helper library to index

---
+++ index/find_crate.mk
@@ -0,0 +1,7 @@
+PACKAGES += find_crate
+pkg_find_crate_name = find_crate
+pkg_find_crate_description = Find Rust libs and exes in Erlang application priv directory
+pkg_find_crate_homepage = https://github.com/goertzenator/find_crate
+pkg_find_crate_fetch = git
+pkg_find_crate_repo = https://github.com/goertzenator/find_crate
+pkg_find_crate_commit = master
---
+++ index/rust.mk
@@ -0,0 +1,7 @@
+PACKAGES += rust
+pkg_rust_name = rust
+pkg_rust_description = Build Rust crates in an Erlang application
+pkg_rust_homepage = https://github.com/goertzenator/rust.mk
+pkg_rust_fetch = git
+pkg_rust_repo = https://github.com/goertzenator/rust.mk
+pkg_rust_commit = master
