525e3d93e46705ba1f7303485744a93de30859d9
Daniel Goertzen <daniel.goertzen@gmail.com>
2016-06-08 13:46:52 -0500

    Fix rust.mk package info.

---
+++ index/rust_mk.mk
@@ -0,0 +1,7 @@
+PACKAGES += rust_mk
+pkg_rust_mk_name = rust_mk
+pkg_rust_mk_description = Build Rust crates in an Erlang application
+pkg_rust_mk_homepage = https://github.com/goertzenator/rust.mk
+pkg_rust_mk_fetch = git
+pkg_rust_mk_repo = https://github.com/goertzenator/rust.mk
+pkg_rust_mk_commit = master
