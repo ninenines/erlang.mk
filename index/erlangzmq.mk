31c6c181b980f60f03cb83f00df3bf37c2d7cda1
Andriy Drozdyuk <drozzy@gmail.com>
2016-07-30 14:35:54 -0400

    Add package erlangzmq to the index

--- index/erlangzmq.mk
+++ index/erlangzmq.mk
@@ -4,4 +4,4 @@ pkg_erlangzmq_description = Native Erlang implemention of ZeroMQ Message Transpo
 pkg_erlangzmq_homepage = http://choven.ca
 pkg_erlangzmq_fetch = git
 pkg_erlangzmq_repo = https://github.com/chovencorp/erlangzmq
-pkg_erlangzmq_commit = 1.1.0
+pkg_erlangzmq_commit = master
