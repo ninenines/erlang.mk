e33275842ef5c0e547ccaa3e4334629547f118a7
Andriy Drozdyuk <drozzy@gmail.com>
2016-09-03 11:27:39 -0400

    Add package chumak to the index

---
+++ index/chumak.mk
@@ -0,0 +1,7 @@
+PACKAGES += chumak
+pkg_chumak_name = chumak
+pkg_chumak_description = Pure Erlang implementation of ZeroMQ Message Transport Protocol.
+pkg_chumak_homepage = http://choven.ca
+pkg_chumak_fetch = git
+pkg_chumak_repo = https://github.com/chovencorp/chumak
+pkg_chumak_commit = 1.1.1
