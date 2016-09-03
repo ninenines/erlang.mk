5c27a25c8051d798a3ff1b02d935d43e9ab909b3
Andriy Drozdyuk <drozzy@gmail.com>
2016-09-03 14:41:01 -0400

    Delete erlangzmq.mk

--- index/erlangzmq.mk
+++
@@ -1,7 +0,0 @@
-PACKAGES += erlangzmq
-pkg_erlangzmq_name = erlangzmq
-pkg_erlangzmq_description = Native Erlang implemention of ZeroMQ Message Transport Protocol.
-pkg_erlangzmq_homepage = http://choven.ca
-pkg_erlangzmq_fetch = git
-pkg_erlangzmq_repo = https://github.com/chovencorp/erlangzmq
-pkg_erlangzmq_commit = master
