a7a059504dd229958d8cd059b2a6ab93b4a6805a
Zaiming Shi <zmstone@gmail.com>
2016-10-01 15:47:34 +0200

    add snappyer to index

---
+++ index/snappyer.mk
@@ -0,0 +1,7 @@
+PACKAGES += snappyer
+pkg_snappyer_name = snappyer
+pkg_snappyer_description = Snappy as nif for Erlang
+pkg_snappyer_homepage = https://github.com/zmstone/snappyer
+pkg_snappyer_fetch = git
+pkg_snappyer_repo = https://github.com/zmstone/snappyer.git
+pkg_snappyer_commit = master
