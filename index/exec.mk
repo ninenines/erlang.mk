9869642cb93e1645b778b1f47850f6c186042043
Adam Cammack <adam_cammack@rapid7.com>
2016-10-17 11:20:48 -0500

    Fix erlexec application name
    erlexec now requires that particular name in order to successfully boot.

--- index/exec.mk
+++ index/exec.mk
@@ -1,5 +1,5 @@
 PACKAGES += exec
-pkg_exec_name = exec
+pkg_exec_name = erlexec
 pkg_exec_description = Execute and control OS processes from Erlang/OTP.
 pkg_exec_homepage = http://saleyn.github.com/erlexec
 pkg_exec_fetch = git
