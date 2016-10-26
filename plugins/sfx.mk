7f148006fdd35992bd90ca24c70b5f0e00a8f719
Loïc Hoguin <essen@ninenines.eu>
2016-10-26 14:35:26 +0300

    Better FreeBSD fix

--- plugins/sfx.mk
+++ plugins/sfx.mk
@@ -24,7 +24,7 @@ ARCHIVE=`awk '/^__ARCHIVE_BELOW__$$/ {print NR + 1; exit 0;}' $$0`
 FILENAME=$$(basename $$0)
 REL=$${FILENAME%.*}
 
-tail -n+$$ARCHIVE $$0 | tar xz -C $$TMPDIR
+tail -n+$$ARCHIVE $$0 | tar -xzf - -C $$TMPDIR
 
 $$TMPDIR/$$REL/bin/$$REL console
 RET=$$?
@@ -38,10 +38,8 @@ endef
 
 sfx:
 	$(call render_template,sfx_stub,$(SFX_OUTPUT))
-	$(gen_verbose) tar -C $(RELX_OUTPUT_DIR) -czf $(RELX_OUTPUT_DIR)/$(RELX_RELEASE).tgz $(RELX_RELEASE)
-	$(verbose) cat $(RELX_OUTPUT_DIR)/$(RELX_RELEASE).tgz >> $(SFX_OUTPUT)
+	$(gen_verbose) tar -C $(RELX_OUTPUT_DIR) -czf - $(RELX_RELEASE) >> $(SFX_OUTPUT)
 	$(verbose) chmod +x $(SFX_OUTPUT)
-	$(verbose) rm $(RELX_OUTPUT_DIR)/$(RELX_RELEASE).tgz
 
 endif
 endif
