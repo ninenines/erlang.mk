0352abfd3c0b6982a7aac19af3c369d930843128
Loïc Hoguin <essen@ninenines.eu>
2016-10-31 12:28:45 +0200

    Add package asciideck to the index

---
+++ index/asciideck.mk
@@ -0,0 +1,7 @@
+PACKAGES += asciideck
+pkg_asciideck_name = asciideck
+pkg_asciideck_description = Asciidoc for Erlang.
+pkg_asciideck_homepage = https://ninenines.eu
+pkg_asciideck_fetch = git
+pkg_asciideck_repo = https://github.com/ninenines/asciideck
+pkg_asciideck_commit = master
