23a862c5618f6f4d7d46958f61f3e78552d5ed39
Stanislaw Klekot <dozzie@jarowit.net>
2017-05-09 15:49:05 +0200

    Add package toml to the index

---
+++ index/toml.mk
@@ -0,0 +1,7 @@
+PACKAGES += toml
+pkg_toml_name = toml
+pkg_toml_description = TOML (0.4.0) config parser
+pkg_toml_homepage = http://dozzie.jarowit.net/trac/wiki/TOML
+pkg_toml_fetch = git
+pkg_toml_repo = https://github.com/dozzie/toml
+pkg_toml_commit = v0.2.0
