ef46b3e863b3d4a9be2a50b626e8da799e707b70
Loïc Hoguin <essen@ninenines.eu>
2017-05-22 11:14:06 +0200

    Add package idna to the index

---
+++ index/idna.mk
@@ -0,0 +1,7 @@
+PACKAGES += idna
+pkg_idna_name = idna
+pkg_idna_description = Erlang IDNA lib
+pkg_idna_homepage = https://github.com/benoitc/erlang-idna
+pkg_idna_fetch = git
+pkg_idna_repo = https://github.com/benoitc/erlang-idna
+pkg_idna_commit = master
