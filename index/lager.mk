b5d014d21d98654cb9e01e9207d5f52201d0fde9
Daniil Fedotov <dfedotov@pivotal.io>
2017-06-08 10:42:36 +0100

    Update lager repository org.
    basho/lager is unmaintained. A while ago, erlang-lager/lager project was started
    to continue progress on lager, including support for recent OTP versions and rebar3.

--- index/lager.mk
+++ index/lager.mk
@@ -1,7 +1,7 @@
 PACKAGES += lager
 pkg_lager_name = lager
 pkg_lager_description = A logging framework for Erlang/OTP.
-pkg_lager_homepage = https://github.com/basho/lager
+pkg_lager_homepage = https://github.com/erlang-lager/lager
 pkg_lager_fetch = git
-pkg_lager_repo = https://github.com/basho/lager
+pkg_lager_repo = https://github.com/erlang-lager/lager
 pkg_lager_commit = master
