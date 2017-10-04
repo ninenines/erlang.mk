e4027c38f92c8679e011d224bfde86d9b9629964
Stanislaw Klekot <dozzie@jarowit.net>
2017-10-04 20:20:06 +0200

    Add package subproc to the index

---
+++ index/subproc.mk
@@ -0,0 +1,7 @@
+PACKAGES += subproc
+pkg_subproc_name = subproc
+pkg_subproc_description = unix subprocess manager with {active,once|false} modes
+pkg_subproc_homepage = http://dozzie.jarowit.net/trac/wiki/subproc
+pkg_subproc_fetch = git
+pkg_subproc_repo = https://github.com/dozzie/subproc
+pkg_subproc_commit = v0.1.0
