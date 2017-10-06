ba7b0fb40da9dfd755bd27e69909ee335cdc9a8e
Olivier Girondel <olivier@biniou.info>
2017-10-06 16:23:52 +0200

    Add package escalus to the index

---
+++ index/escalus.mk
@@ -0,0 +1,7 @@
+PACKAGES += escalus
+pkg_escalus_name = escalus
+pkg_escalus_description = An XMPP client library in Erlang for conveniently testing XMPP servers
+pkg_escalus_homepage = https://github.com/esl/escalus
+pkg_escalus_fetch = git
+pkg_escalus_repo = https://github.com/esl/escalus
+pkg_escalus_commit = master
