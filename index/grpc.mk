45521311e811cd0fe2cb9cc25f878abbf5443092
willemdj <w.a.de.jong@gmail.com>
2017-11-25 14:35:07 +0100

    Add package grpc to the index

---
+++ index/grpc.mk
@@ -0,0 +1,7 @@
+PACKAGES += grpc
+pkg_grpc_name = grpc
+pkg_grpc_description = gRPC server in Erlang
+pkg_grpc_homepage = https://github.com/Bluehouse-Technology/grpc
+pkg_grpc_fetch = git
+pkg_grpc_repo = https://github.com/Bluehouse-Technology/grpc
+pkg_grpc_commit = master
