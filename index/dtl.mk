2e4c6fe5d0754d7d2f04ba1de6d58d6a550dc0e2
Loïc Hoguin <essen@ninenines.eu>
2017-12-16 11:00:36 +0100

    Remove classifier, dtl and gossiperl
    The first two are gone, the latter depends on a dependency
    that's gone and the project hasn't been maintained for 3 years.

--- index/dtl.mk
+++
@@ -1,7 +0,0 @@
-PACKAGES += dtl
-pkg_dtl_name = dtl
-pkg_dtl_description = Django Template Language: A full-featured port of the Django template engine to Erlang.
-pkg_dtl_homepage = https://github.com/oinksoft/dtl
-pkg_dtl_fetch = git
-pkg_dtl_repo = https://github.com/oinksoft/dtl
-pkg_dtl_commit = master
--- index/gossiperl.mk
+++
@@ -1,7 +0,0 @@
-PACKAGES += gossiperl
-pkg_gossiperl_name = gossiperl
-pkg_gossiperl_description = Gossip middleware in Erlang
-pkg_gossiperl_homepage = http://gossiperl.com/
-pkg_gossiperl_fetch = git
-pkg_gossiperl_repo = https://github.com/gossiperl/gossiperl
-pkg_gossiperl_commit = master
