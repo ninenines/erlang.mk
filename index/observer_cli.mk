2e848f337b68fa70b6e9097ce747894146f4141b
Loïc Hoguin <essen@ninenines.eu>
2018-03-28 11:39:50 +0200

    Add package observer_cli to the index

---
+++ index/observer_cli.mk
@@ -0,0 +1,7 @@
+PACKAGES += observer_cli
+pkg_observer_cli_name = observer_cli
+pkg_observer_cli_description = Visualize Erlang/Elixir Nodes On The Command Line
+pkg_observer_cli_homepage = http://zhongwencool.github.io/observer_cli
+pkg_observer_cli_fetch = git
+pkg_observer_cli_repo = https://github.com/zhongwencool/observer_cli
+pkg_observer_cli_commit = master
