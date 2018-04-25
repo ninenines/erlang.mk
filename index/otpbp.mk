9245b41f6ceea8b8dacafed5e2fc455a8ae78f88
Loïc Hoguin <essen@ninenines.eu>
2018-04-25 18:41:11 +0200

    Add package otpbp to the index

---
+++ index/otpbp.mk
@@ -0,0 +1,7 @@
+PACKAGES += otpbp
+pkg_otpbp_name = otpbp
+pkg_otpbp_description = Parse transformer for use new OTP functions in old Erlang/OTP releases (R15, R16, 17, 18, 19)
+pkg_otpbp_homepage = https://github.com/Ledest/otpbp
+pkg_otpbp_fetch = git
+pkg_otpbp_repo = https://github.com/Ledest/otpbp
+pkg_otpbp_commit = master
