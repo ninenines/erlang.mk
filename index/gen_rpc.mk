7f737600d75497379a19f151cfa6345dfb62f10e
Stephane Bourque <stephane.bourque@gmail.com>
2018-12-07 11:13:57 -0600

    Add package gen_rpc to the index

---
+++ index/gen_rpc.mk
@@ -0,0 +1,7 @@
+PACKAGES += gen_rpc
+pkg_gen_rpc_name = gen_rpc
+pkg_gen_rpc_description = A scalable RPC library for Erlang-VM based languages
+pkg_gen_rpc_homepage = https://github.com/priestjim/gen_rpc.git
+pkg_gen_rpc_fetch = git
+pkg_gen_rpc_repo = https://github.com/priestjim/gen_rpc.git
+pkg_gen_rpc_commit = master
