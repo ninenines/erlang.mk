b264034d2f1c02e037e8489fdc553ec57ab942de
Thomas Elsgaard <thomas.elsgaard@timezone4.com>
2018-12-29 19:27:58 +0100

    Add package PGO to index

---
+++ index/pgo.mk
@@ -0,0 +1,7 @@
+PACKAGES += pgo
+pkg_pgo_name = pgo
+pkg_pgo_description = Erlang Postgres client and connection pool
+pkg_pgo_homepage = https://github.com/erleans/pgo.git
+pkg_pgo_fetch = git
+pkg_pgo_repo = https://github.com/erleans/pgo.git
+pkg_pgo_commit = master
