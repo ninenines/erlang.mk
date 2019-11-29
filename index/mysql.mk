fc6956d71cced599e025bc09027f78336031c0ba
juhlig <j.uhlig@mailingwork.de>
2019-11-29 12:02:52 +0100

    Remove package mysql

--- index/mysql.mk
+++
@@ -1,7 +0,0 @@
-PACKAGES += mysql
-pkg_mysql_name = mysql
-pkg_mysql_description = Erlang MySQL Driver (from code.google.com)
-pkg_mysql_homepage = https://github.com/dizzyd/erlang-mysql-driver
-pkg_mysql_fetch = git
-pkg_mysql_repo = https://github.com/dizzyd/erlang-mysql-driver
-pkg_mysql_commit = master
