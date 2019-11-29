c34e48f23d8bf6fad70e3fd67f5d516815557e4f
juhlig <j.uhlig@mailingwork.de>
2019-11-29 17:23:58 +0100

    Add mysql-otp to the index

---
+++ index/mysql.mk
@@ -0,0 +1,7 @@
+PACKAGES += mysql
+pkg_mysql_name = mysql
+pkg_mysql_description = MySQL client library for Erlang/OTP
+pkg_mysql_homepage = https://github.com/mysql-otp/mysql-otp
+pkg_mysql_fetch = git
+pkg_mysql_repo = https://github.com/mysql-otp/mysql-otp
+pkg_mysql_commit = 1.5.1
