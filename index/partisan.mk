034582668fc5ac9c6c8d0fffaa845a635d1f71f3
Loïc Hoguin <essen@ninenines.eu>
2020-02-11 11:01:23 +0100

    Add package partisan to the index

---
+++ index/partisan.mk
@@ -0,0 +1,7 @@
+PACKAGES += partisan
+pkg_partisan_name = partisan
+pkg_partisan_description = High-performance, high-scalability distributed computing with Erlang and Elixir.
+pkg_partisan_homepage = http://partisan.cloud
+pkg_partisan_fetch = git
+pkg_partisan_repo = https://github.com/lasp-lang/partisan
+pkg_partisan_commit = master
