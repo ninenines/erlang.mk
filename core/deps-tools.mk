5c78b3facfbde2e052c43cc1cc6e8d6208b896e4
Gerhard Lazu <gerhard@lazu.co.uk>
2020-02-19 10:20:58 +0000

    Prepend " DEPI  " to all lines that list deps info
    This keeps the similarity to GEN & DEP info lines.
    
    Decided to not remove duplicates since different deps may define
    different versions for the same dep. The simplest thing is to list info
    for all deps, as they appear, no deduping.
    
    Signed-off-by: Gerhard Lazu <gerhard@lazu.co.uk>

--- core/deps-tools.mk
+++ core/deps-tools.mk
@@ -103,47 +103,5 @@ list-deps list-doc-deps list-rel-deps list-test-deps list-shell-deps:
 list-deps-info: $(ALL_DEPS_DIRS:%=%-list-deps-info)
 
 %-list-deps-info: $(ERLANG_MK_RECURSIVE_DEPS_LIST)
-	$(verbose) echo "$* $(call dep_commit,$(notdir $*)) $(call dep_repo,$(notdir $*))"
-	$(verbose) $(MAKE) --no-print-directory --directory $* list-deps-info IS_DEP=1 \
-	|| echo "$* is missing list-deps-info target, skipping..."
-
-# For dep_rabbitmq_management = git https://github.com/rabbitmq/rabbitmq-management v3.8.2
-# The current implementation produces the following output:
-# - /Users/gerhard/github.com/rabbitmq/LicenseFinder/features/fixtures/erlangmk/deps/amqp_client v3.8.2 rabbitmq-erlang-client
-# + /Users/gerhard/github.com/rabbitmq/LicenseFinder/features/fixtures/erlangmk/deps/amqp_client v3.8.2 https://github.com/rabbitmq/rabbitmq-erlang-client
-# + /Users/gerhard/github.com/rabbitmq/LicenseFinder/features/fixtures/erlangmk/deps/aten 0.5.2 https://hex.pm/packages/aten
-#   /Users/gerhard/github.com/rabbitmq/LicenseFinder/features/fixtures/erlangmk/deps/cowboy 2.6.1 https://hex.pm/packages/cowboy
-#   /Users/gerhard/github.com/rabbitmq/LicenseFinder/features/fixtures/erlangmk/deps/cowlib 2.7.0 https://github.com/ninenines/cowlib
-# - /Users/gerhard/github.com/rabbitmq/LicenseFinder/features/fixtures/erlangmk/deps/cowlib 2.7.0 https://github.com/ninenines/cowlib
-# + /Users/gerhard/github.com/rabbitmq/LicenseFinder/features/fixtures/erlangmk/deps/credentials_obfuscation 1.1.0 https://hex.pm/packages/credentials_obfuscation
-# + /Users/gerhard/github.com/rabbitmq/LicenseFinder/features/fixtures/erlangmk/deps/gen_batch_server 0.8.2 https://hex.pm/packages/gen_batch_server
-# + /Users/gerhard/github.com/rabbitmq/LicenseFinder/features/fixtures/erlangmk/deps/goldrush 0.1.9 https://github.com/DeadZen/goldrush.git
-# + /Users/gerhard/github.com/rabbitmq/LicenseFinder/features/fixtures/erlangmk/deps/jsx 2.9.0 https://hex.pm/packages/jsx
-# + /Users/gerhard/github.com/rabbitmq/LicenseFinder/features/fixtures/erlangmk/deps/lager 3.8.0 https://hex.pm/packages/lager
-# + /Users/gerhard/github.com/rabbitmq/LicenseFinder/features/fixtures/erlangmk/deps/observer_cli 1.5.2 https://hex.pm/packages/observer_cli
-# + /Users/gerhard/github.com/rabbitmq/LicenseFinder/features/fixtures/erlangmk/deps/ra 1.0.5 https://hex.pm/packages/ra
-# - /Users/gerhard/github.com/rabbitmq/LicenseFinder/features/fixtures/erlangmk/deps/rabbit v3.8.2 rabbitmq-server
-# + /Users/gerhard/github.com/rabbitmq/LicenseFinder/features/fixtures/erlangmk/deps/rabbit v3.8.2 https://github.com/rabbitmq/rabbitmq-server
-# - /Users/gerhard/github.com/rabbitmq/LicenseFinder/features/fixtures/erlangmk/deps/rabbit_common v3.8.2 rabbitmq-common
-# + /Users/gerhard/github.com/rabbitmq/LicenseFinder/features/fixtures/erlangmk/deps/rabbit_common v3.8.2 https://github.com/rabbitmq/rabbitmq-common
-# + /Users/gerhard/github.com/rabbitmq/LicenseFinder/features/fixtures/erlangmk/deps/rabbitmq_cli v3.8.2 https://github.com/rabbitmq/rabbitmq-cli
-# + /Users/gerhard/github.com/rabbitmq/LicenseFinder/features/fixtures/erlangmk/deps/rabbitmq_codegen v3.8.2 https://github.com/rabbitmq/rabbitmq-codegen
-#   /Users/gerhard/github.com/rabbitmq/LicenseFinder/features/fixtures/erlangmk/deps/rabbitmq_management v3.8.2 https://github.com/rabbitmq/rabbitmq-management
-# - /Users/gerhard/github.com/rabbitmq/LicenseFinder/features/fixtures/erlangmk/deps/rabbitmq_management_agent v3.8.2 rabbitmq-management-agent
-# + /Users/gerhard/github.com/rabbitmq/LicenseFinder/features/fixtures/erlangmk/deps/rabbitmq_management_agent v3.8.2 https://github.com/rabbitmq/rabbitmq-management-agent
-# - /Users/gerhard/github.com/rabbitmq/LicenseFinder/features/fixtures/erlangmk/deps/rabbitmq_web_dispatch v3.8.2 rabbitmq-web-dispatch
-# + /Users/gerhard/github.com/rabbitmq/LicenseFinder/features/fixtures/erlangmk/deps/rabbitmq_web_dispatch v3.8.2 https://github.com/rabbitmq/rabbitmq-web-dispatch
-#   /Users/gerhard/github.com/rabbitmq/LicenseFinder/features/fixtures/erlangmk/deps/ranch 1.7.1 https://hex.pm/packages/ranch
-# - /Users/gerhard/github.com/rabbitmq/LicenseFinder/features/fixtures/erlangmk/deps/ranch 1.7.1 https://hex.pm/packages/ranch
-# + /Users/gerhard/github.com/rabbitmq/LicenseFinder/features/fixtures/erlangmk/deps/recon 2.5.0 https://hex.pm/packages/recon
-# + /Users/gerhard/github.com/rabbitmq/LicenseFinder/features/fixtures/erlangmk/deps/stdout_formatter 0.2.2 https://hex.pm/packages/stdout_formatter
-# + /Users/gerhard/github.com/rabbitmq/LicenseFinder/features/fixtures/erlangmk/deps/syslog 3.4.5 https://github.com/schlagert/syslog
-# + /Users/gerhard/github.com/rabbitmq/LicenseFinder/features/fixtures/erlangmk/deps/sysmon_handler 1.2.0 https://hex.pm/packages/sysmon_handler
-#
-# - duplicate or incorrect line
-# + missing line
-#
-# TODO:
-# - Remove duplicates from list-deps-info
-# - Add list-deps-info targets to https://github.com/rabbitmq/rabbitmq-common/mk/rabbitmq-tools.mk
-# - Overwrite dep_repo in rabbitmq-tools.mk so that it handles git_rmq
+	$(verbose) echo " DEPI   $* $(call dep_commit,$(notdir $*)) $(call dep_repo,$(notdir $*))"
+	$(verbose) $(MAKE) --no-print-directory --directory $* list-deps-info IS_DEP=1 || true
