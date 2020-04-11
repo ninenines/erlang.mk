c08f50863d901e7ed8834e2d53159aa33e2c06b9
crownedgrouse <eric.pailleau@wanadoo.fr>
2020-04-11 15:20:16 +0200

    Add package esh.mk to the index

---
+++ index/esh.mk.mk
@@ -0,0 +1,7 @@
+PACKAGES += esh.mk
+pkg_esh.mk_name = esh.mk
+pkg_esh.mk_description = esh template engine plugin for erlang.mk
+pkg_esh.mk_homepage = https://github.com/crownedgrouse/esh.mk
+pkg_esh.mk_fetch = git
+pkg_esh.mk_repo = https://github.com/crownedgrouse/esh.mk
+pkg_esh.mk_commit = master
