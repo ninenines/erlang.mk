da07f52faa5289f0220fdf55fef6311110c488c5
crownedgrouse <eric.pailleau@wanadoo.fr>
2020-04-11 15:29:10 +0200

    Add plugin in documentation plugin list

---
+++ index/esh_mk.mk
@@ -0,0 +1,7 @@
+PACKAGES += esh_mk
+pkg_esh_mk_name = esh_mk
+pkg_esh_mk_description = esh template engine plugin for erlang.mk
+pkg_esh_mk_homepage = https://github.com/crownedgrouse/esh.mk
+pkg_esh_mk_fetch = git
+pkg_esh_mk_repo = https://github.com/crownedgrouse/esh.mk.git
+pkg_esh_mk_commit = master
