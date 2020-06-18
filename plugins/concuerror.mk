9de378e9b0489775a41dedea085e9d77ca52e256
Loïc Hoguin <essen@ninenines.eu>
2020-06-18 15:02:43 +0200

    Add initial Concuerror integration
    https://concuerror.com/

---
+++ plugins/concuerror.mk
@@ -0,0 +1,67 @@
+# Copyright (c) 2020, Loïc Hoguin <essen@ninenines.eu>
+# This file is part of erlang.mk and subject to the terms of the ISC License.
+
+ifdef CONCUERROR_TESTS
+
+.PHONY: concuerror distclean-concuerror
+
+# Configuration
+
+CONCUERROR_LOGS_DIR ?= $(CURDIR)/logs
+CONCUERROR_OPTS ?=
+
+# Core targets.
+
+check:: concuerror
+
+ifndef KEEP_LOGS
+distclean:: distclean-concuerror
+endif
+
+# Plugin-specific targets.
+
+$(ERLANG_MK_TMP)/Concuerror/bin/concuerror: | $(ERLANG_MK_TMP)
+	$(verbose) git clone https://github.com/parapluu/Concuerror $(ERLANG_MK_TMP)/Concuerror
+	$(verbose) make -C $(ERLANG_MK_TMP)/Concuerror
+
+$(CONCUERROR_LOGS_DIR):
+	$(verbose) mkdir -p $(CONCUERROR_LOGS_DIR)
+
+define concuerror_html_report
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Concuerror HTML report</title>
+</head>
+<body>
+<h1>Concuerror HTML report</h1>
+<p>Generated on $(concuerror_date)</p>
+<ul>
+$(foreach t,$(concuerror_targets),<li><a href="$(t).txt">$(t)</a></li>)
+</ul>
+</body>
+</html>
+endef
+
+concuerror: $(addprefix concuerror-,$(subst :,-,$(CONCUERROR_TESTS)))
+	$(eval concuerror_date := $(shell date))
+	$(eval concuerror_targets := $^)
+	$(verbose) $(call core_render,concuerror_html_report,$(CONCUERROR_LOGS_DIR)/concuerror.html)
+
+define concuerror_target
+.PHONY: concuerror-$1-$2
+
+concuerror-$1-$2: test-build | $(ERLANG_MK_TMP)/Concuerror/bin/concuerror $(CONCUERROR_LOGS_DIR)
+	$(ERLANG_MK_TMP)/Concuerror/bin/concuerror \
+		--pa $(CURDIR)/ebin --pa $(TEST_DIR) \
+		-o $(CONCUERROR_LOGS_DIR)/concuerror-$1-$2.txt \
+		$$(CONCUERROR_OPTS) -m $1 -t $2
+endef
+
+$(foreach test,$(CONCUERROR_TESTS),$(eval $(call concuerror_target,$(firstword $(subst :, ,$(test))),$(lastword $(subst :, ,$(test))))))
+
+distclean-concuerror:
+	$(gen_verbose) rm -rf $(CONCUERROR_LOGS_DIR)
+
+endif
---
+++ test/plugin_concuerror.mk
@@ -0,0 +1,39 @@
+# Concuerror plugin.
+
+CONCUERROR_TARGETS = $(call list_targets,concuerror)
+
+.PHONY: concuerror $(CONCUERROR_TARGETS)
+
+concuerror: $(CONCUERROR_TARGETS)
+
+concuerror-app: init
+
+	$i "Bootstrap a new OTP application named $(APP)"
+	$t mkdir $(APP)/
+	$t cp ../erlang.mk $(APP)/
+	$t $(MAKE) -C $(APP) -f erlang.mk bootstrap $v
+
+	$i "Create a test module with a function that returns immediately"
+	$t mkdir $(APP)/test
+	$t printf "%s\n" \
+		"-module(concuerror_success)." \
+		"-export([test/0])." \
+		"test() -> ok." > $(APP)/test/concuerror_success.erl
+
+	$i "Add the test case to CONCUERROR_TESTS"
+	$t perl -ni.bak -e 'print;if ($$.==1) {print "CONCUERROR_TESTS += concuerror_success:test\n"}' $(APP)/Makefile
+
+	$i "Confirm that Concuerror completes successfully"
+	$t $(MAKE) -C $(APP) concuerror $v
+
+	$i "Create a test module with a function that has no local return"
+	$t printf "%s\n" \
+		"-module(concuerror_error)." \
+		"-export([test/0])." \
+		"test() -> 1 = 2, ok." > $(APP)/test/concuerror_error.erl
+
+	$i "Add the test case to CONCUERROR_TESTS"
+	$t perl -ni.bak -e 'print;if ($$.==1) {print "CONCUERROR_TESTS += concuerror_error:test\n"}' $(APP)/Makefile
+
+	$i "Confirm that Concuerror errors out"
+	$t ! $(MAKE) -C $(APP) concuerror $v
