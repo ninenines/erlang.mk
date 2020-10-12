860586fb10b5dfeae04916c1af46327d1186a979
juhlig <j.uhlig@mailingwork.de>
2020-10-12 13:24:10 +0200

    Update mysql-otp version to 1.7.0

--- index/mysql.mk
+++ index/mysql.mk
@@ -4,4 +4,4 @@ pkg_mysql_description = MySQL client library for Erlang/OTP
 pkg_mysql_homepage = https://github.com/mysql-otp/mysql-otp
 pkg_mysql_fetch = git
 pkg_mysql_repo = https://github.com/mysql-otp/mysql-otp
-pkg_mysql_commit = 1.5.1
+pkg_mysql_commit = 1.7.0
