b9676cb3fd1f71fef34d5f151baba100d77072d4
Martin Björklund <mbj4668@gmail.com>
2020-11-26 09:46:04 +0100

    Ignore files with '#' in their names, e.g. emacs lock files
    Also remove usage of grep for ignoring files since find
    can do this directly.

--- test/core_makedep.mk
+++ test/core_makedep.mk
@@ -25,6 +25,26 @@ core-makedep-behavior: init
 	$t test -f $(APP)/ebin/boy.beam
 	$t test -f $(APP)/ebin/human.beam
 
+core-makedep-ignore-special-files: init
+
+	$i "Bootstrap a new OTP library named $(APP)"
+	$t mkdir $(APP)/
+	$t cp ../erlang.mk $(APP)/
+	$t $(MAKE) -C $(APP) -f erlang.mk bootstrap-lib $v
+	$t mkdir $(APP)/src/core
+
+	$i "Generate related .erl files"
+	$t printf "%s\n" "-module(human)." "-export([live/0])." "live() -> ok." > $(APP)/src/core/human.erl
+	$t printf "%s\n" "-module(boy)." "-import(human,[live/0])." > $(APP)/src/boy.erl
+	$t printf "%s\n" "-module(boy)." "-import(human,[live/0])." > $(APP)/src/.#boy.erl
+	$t $(MAKE) -C $(APP) $v
+
+	$i "Check that all compiled files exist"
+	$t test -f $(APP)/$(APP).d
+	$t test -f $(APP)/ebin/$(APP).app
+	$t test -f $(APP)/ebin/boy.beam
+	$t test -f $(APP)/ebin/human.beam
+
 core-makedep-import: init
 
 	$i "Bootstrap a new OTP library named $(APP)"
