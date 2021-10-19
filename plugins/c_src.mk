94718f7715a05087d966a0ca4b32527892ac6cfc
Dominic Letz <dominic@diode.io>
2021-10-19 23:17:33 +0200

    Removed hardcoded `-arch x86_64` for MacOS nifs
    For current macs the flag seems problematic as it overrides
    the default choice of building for the current architecture
    and hence prevents building on ARM based macs.

--- plugins/c_src.mk
+++ plugins/c_src.mk
@@ -35,9 +35,9 @@ ifeq ($(PLATFORM),msys2)
 	CXXFLAGS ?= -O3 -finline-functions -Wall
 else ifeq ($(PLATFORM),darwin)
 	CC ?= cc
-	CFLAGS ?= -O3 -std=c99 -arch x86_64 -Wall -Wmissing-prototypes
-	CXXFLAGS ?= -O3 -arch x86_64 -Wall
-	LDFLAGS ?= -arch x86_64 -flat_namespace -undefined suppress
+	CFLAGS ?= -O3 -std=c99 -Wall -Wmissing-prototypes
+	CXXFLAGS ?= -O3 -Wall
+	LDFLAGS ?= -flat_namespace -undefined suppress
 else ifeq ($(PLATFORM),freebsd)
 	CC ?= cc
 	CFLAGS ?= -O3 -std=c99 -finline-functions -Wall -Wmissing-prototypes
