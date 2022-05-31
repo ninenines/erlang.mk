28d3d7d7662999405aabd4dd330902f3cbf0920b
Loïc Hoguin <essen@ninenines.eu>
2022-05-31 11:33:29 +0200

    Point Relx back to upstream
    All PRs have been merged.

--- index/relx.mk
+++ index/relx.mk
@@ -3,5 +3,5 @@ pkg_relx_name = relx
 pkg_relx_description = Sane, simple release creation for Erlang
 pkg_relx_homepage = https://github.com/erlware/relx
 pkg_relx_fetch = git
-pkg_relx_repo = https://github.com/essen/relx
+pkg_relx_repo = https://github.com/erlware/relx
 pkg_relx_commit = main
