52492e67440e4cf7c00ff0dd892cd911b366ce2f
Loïc Hoguin <essen@ninenines.eu>
2023-05-10 09:44:30 +0200

    Remove packages that use the apps/ structure
    They cannot be used as dependencies directly.

--- index/rebar.mk
+++
@@ -1,7 +0,0 @@
-PACKAGES += rebar
-pkg_rebar_name = rebar
-pkg_rebar_description = Erlang build tool that makes it easy to compile and test Erlang applications, port drivers and releases.
-pkg_rebar_homepage = http://www.rebar3.org
-pkg_rebar_fetch = git
-pkg_rebar_repo = https://github.com/erlang/rebar3
-pkg_rebar_commit = main
