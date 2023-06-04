4efc8e2ea4c782e5b5fdae0c5bcb62497b9396e7
Tyler Hughes <artman41@gmail.com>
2023-06-04 23:37:03 +0100

    Fix tests

--- test/core_elixir.mk
+++ test/core_elixir.mk
@@ -8,7 +8,7 @@ CORE_ELIXIR_TARGETS = $(call list_targets,core-elixir)
 
 core-elixir: $(CORE_ELIXIR_TARGETS)
 
-core-test-project: init
+core-elixir-test-project: init
 
 	$i "Bootstrap a new OTP library named $(APP)"
 	$t mkdir $(APP)/
@@ -16,12 +16,14 @@ core-test-project: init
 	$t $(MAKE) -C $(APP) -f erlang.mk bootstrap $v
 
 	$i "Configure Makefile"
-	$i echo "ERLC_OPTS += +debug_info +'{parse_transform,lager_transform}'" >> $(APP)/Makefile
-	$i echo "DEPS += lager" >> $(APP)/Makefile
-	$i echo "DEPS += jason" >> $(APP)/Makefile
-	$i echo "DEPS += phoenix" >> $(APP)/Makefile
-	$i echo "dep_jason = git https://github.com/michalmuskala/jason.git master" >> $(APP)/Makefile
-	$i echo "dep_phoenix = hex 1.7.2" >> $(APP)/Makefile
+	$t echo "ERLC_OPTS += +debug_info +'{parse_transform,lager_transform}'" >> $(APP)/Makefile
+	$t echo "DEPS += lager" >> $(APP)/Makefile
+	$t echo "DEPS += jason" >> $(APP)/Makefile
+	$t echo "DEPS += phoenix" >> $(APP)/Makefile
+	$t echo "dep_jason = git https://github.com/michalmuskala/jason.git master" >> $(APP)/Makefile
+	$t echo "dep_phoenix = hex 1.7.2" >> $(APP)/Makefile
+	$t echo "$$(grep -v 'include erlang.mk' $(APP)/Makefile)" > $(APP)/Makefile
+	$t echo "include erlang.mk" >> $(APP)/Makefile
 
 	$i "Make deps"
 	$t $(MAKE) -C $(APP) deps $v
@@ -35,9 +37,9 @@ core-test-project: init
 	$t $(MAKE) -C $(APP) app $v
 
 	$i "Get started apps"
-	$t $(ERL) -pa $(APP)/ebin/ $(APP)/apps/*/ebin/ -eval " \
-		{ok, Apps} = application:ensure_all_started(exte), 
-		true = lists:member(lager, Apps),
-		true = lists:member(jason, Apps),
-		true = lists:member(phoenix, Apps),
-		halt()"
+	$t $(MAKE) -C $(APP) shell SHELL_OPTS="$(filter-out erl,$(ERL)) -pa $(APP)/deps/*/ebin/ $(APP)/ebin/ $(APP)/apps/*/ebin/ -eval \" \
+		{ok, Apps} = application:ensure_all_started('$(APP)'), \
+		true = lists:member(lager, Apps), \
+		true = lists:member(jason, Apps), \
+		true = lists:member(phoenix, Apps), \
+		halt()\""
