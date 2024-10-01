adfd4c28ad0339343b490a50840c23315a0df296
Loïc Hoguin <essen@ninenines.eu>
2024-10-01 14:54:01 +0200

    Update cuttlefish package branch from master to main

--- index/cuttlefish.mk
+++ index/cuttlefish.mk
@@ -4,4 +4,4 @@ pkg_cuttlefish_description = cuttlefish configuration abstraction
 pkg_cuttlefish_homepage = https://github.com/Kyorai/cuttlefish
 pkg_cuttlefish_fetch = git
 pkg_cuttlefish_repo = https://github.com/Kyorai/cuttlefish
-pkg_cuttlefish_commit = master
+pkg_cuttlefish_commit = main
