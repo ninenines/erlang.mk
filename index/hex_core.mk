9376f53f549c05cf692d0e259763049a039e96eb
Loïc Hoguin <essen@ninenines.eu>
2024-11-19 17:10:07 +0100

    Update hex_core to current master
    This includes the SSL security fix for OTP-25+.

--- index/hex_core.mk
+++ index/hex_core.mk
@@ -5,4 +5,4 @@ pkg_hex_core_homepage = https://github.com/hexpm/hex_core
 pkg_hex_core_fetch = git
 HEX_CORE_GIT ?= https://github.com/hexpm/hex_core
 pkg_hex_core_repo = $(HEX_CORE_GIT)
-pkg_hex_core_commit = v0.10.1
+pkg_hex_core_commit = e57b4fb15cde710b3ae09b1d18f148f6999a63cc
