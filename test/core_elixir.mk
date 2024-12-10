ca7a7777cbfacbca30bc0e01a827593b65b0968d
Loïc Hoguin <essen@ninenines.eu>
2024-12-10 11:42:28 +0100

    Rework tests

--- test/core_elixir.mk
+++ test/core_elixir.mk
@@ -8,153 +8,144 @@ CORE_ELIXIR_TARGETS = $(call list_targets,core-elixir)
 
 core-elixir: $(CORE_ELIXIR_TARGETS)
 
-# @todo This one doesn't work with LEGACY=1
-core-elixir-test-project_library: init
+core-elixir-from-dep: init
+
 	$i "Bootstrap a new OTP library named $(APP)"
 	$t mkdir $(APP)/
 	$t cp ../erlang.mk $(APP)/
-	$t $(MAKE) -C $(APP) -f erlang.mk bootstrap $v
+	$t $(MAKE) -C $(APP) -f erlang.mk bootstrap-lib $v
 
-	$i "Configure Makefile"
-	$t echo "ERLC_OPTS += +debug_info +'{parse_transform,lager_transform}'" >> $(APP)/Makefile
-	$t echo "DEPS += elixir" >> $(APP)/Makefile
-	$t echo "DEPS += lager" >> $(APP)/Makefile
-	$t echo "DEPS += jason" >> $(APP)/Makefile
-	$t echo "DEPS += phoenix" >> $(APP)/Makefile
-	$t echo "dep_elixir_commit = v1.17.3" >> $(APP)/Makefile
-	$t echo "dep_lager = git https://github.com/erlang-lager/lager master" >> $(APP)/Makefile
-	$t echo "dep_jason = git https://github.com/michalmuskala/jason.git master" >> $(APP)/Makefile
-	$t echo "dep_phoenix = hex 1.7.2" >> $(APP)/Makefile
-	$t echo "$$(grep -v 'include erlang.mk' $(APP)/Makefile)" > $(APP)/Makefile
-	$t echo "include erlang.mk" >> $(APP)/Makefile
+	$i "Add Elixir, Lager, Jason, Phoenix to the list of dependencies"
+	$t perl -ni.bak -e 'print;if ($$.==1) {print "DEPS = elixir lager jason phoenix\ndep_elixir_commit = v1.17.3\ndep_lager = git https://github.com/erlang-lager/lager master\ndep_jason = git https://github.com/michalmuskala/jason.git master\ndep_phoenix = hex 1.7.2\n"}' $(APP)/Makefile
+
+	$i "Add the lager_transform parse_transform to ERLC_OPTS"
+	$t echo "ERLC_OPTS += +'{parse_transform, lager_transform}'" >> $(APP)/Makefile
 
 ifdef LEGACY
 	$i "Add Elixir, Lager, Jason and Phoenix to the applications key in the .app.src file"
 	$t perl -ni.bak -e 'print;if ($$.==7) {print "\t\telixir,\n\t\tlager,\n\t\tjason,\n\t\tphoenix,\n"}' $(APP)/src/$(APP).app.src
 endif
 
-	$i "Make deps"
-	$t $(MAKE) -C $(APP) deps $v
+	$i "Build the application"
+	$t $(MAKE) -C $(APP) $v
 
-	$i "Check deps have compiled"
-	$t test -d $(APP)/deps/lager/ebin
-	$t test -d $(APP)/deps/jason/ebin
-	$t test -d $(APP)/deps/phoenix/ebin
+	$i "Check that all dependencies were fetched and built"
+	$t test -f $(APP)/deps/elixir/ebin/dep_built
+	$t test -f $(APP)/deps/lager/ebin/dep_built
+	$t test -f $(APP)/deps/jason/ebin/dep_built
+	$t test -f $(APP)/deps/phoenix/ebin/dep_built
 
-	$i "Make the app"
-	$t $(MAKE) -C $(APP) app $v
-
-	$i "Get started apps"
-	$t $(MAKE) -C $(APP) shell SHELL_OPTS="$(filter-out erl,$(ERL)) -pa $(APP)/deps/*/ebin/ $(APP)/ebin/ $(APP)/apps/*/ebin/ -eval \" \
+	$i "Check that the application was compiled correctly"
+	$t $(ERL) -pa $(APP)/ebin/ -pa $(APP)/deps/*/ebin -pa $(APP)/deps/elixir/lib/*/ebin -eval " \
 		{ok, Apps} = application:ensure_all_started('$(APP)'), \
+		true = lists:member(elixir, Apps), \
 		true = lists:member(lager, Apps), \
 		true = lists:member(jason, Apps), \
 		true = lists:member(phoenix, Apps), \
-		halt()\""
+		halt()"
 
-	$i "Check modules aren't duplicated"
-	$t $(MAKE) -C $(APP) shell SHELL_OPTS="$(filter-out erl,$(ERL)) -pa $(APP)/deps/*/ebin/ $(APP)/ebin/ $(APP)/apps/*/ebin/ -eval \" \
-		{ok, Apps} = application:ensure_all_started('$(APP)'), \
-		[begin \
-			{ok, Mods} = application:get_key(App, modules), \
-			true = lists:sort(Mods) =:= lists:usort(Mods) \
-		end || App <- Apps], \
-		halt()\""
+	$i "Check that the Jason application depends on Elixir builtins"
+	$t $(ERL) -pa $(APP)/ebin/ -pa $(APP)/deps/*/ebin -pa $(APP)/deps/elixir/lib/*/ebin -eval " \
+		{ok, Apps} = application:ensure_all_started(jason), \
+		true = lists:member(elixir, Apps), \
+		true = lists:member(eex, Apps), \
+		true = lists:member(logger, Apps), \
+		true = lists:member(mix, Apps), \
+		halt()"
 
-core-elixir-test-project_system: init
-# @todo
-ifneq ($(shell which elixirc),)
+core-elixir-from-system: init
+
 	$i "Bootstrap a new OTP library named $(APP)"
 	$t mkdir $(APP)/
 	$t cp ../erlang.mk $(APP)/
-	$t $(MAKE) -C $(APP) -f erlang.mk bootstrap $v
+	$t $(MAKE) -C $(APP) -f erlang.mk bootstrap-lib $v
 
-	$i "Configure Makefile"
-	$t echo "ERLC_OPTS += +debug_info +'{parse_transform,lager_transform}'" >> $(APP)/Makefile
-	$t echo "DEPS += lager" >> $(APP)/Makefile
-	$t echo "DEPS += jason" >> $(APP)/Makefile
-	$t echo "DEPS += phoenix" >> $(APP)/Makefile
-	$t echo "dep_lager = git https://github.com/erlang-lager/lager master" >> $(APP)/Makefile
-	$t echo "dep_jason = git https://github.com/michalmuskala/jason.git master" >> $(APP)/Makefile
-	$t echo "dep_phoenix = hex 1.7.2" >> $(APP)/Makefile
-	$t echo "$$(grep -v 'include erlang.mk' $(APP)/Makefile)" > $(APP)/Makefile
-	$t echo "include erlang.mk" >> $(APP)/Makefile
+	$i "Add Lager, Jason, Phoenix to the list of dependencies"
+	$t perl -ni.bak -e 'print;if ($$.==1) {print "DEPS = lager jason phoenix\ndep_lager = git https://github.com/erlang-lager/lager master\ndep_jason = git https://github.com/michalmuskala/jason.git master\ndep_phoenix = hex 1.7.2\n"}' $(APP)/Makefile
 
-	$i "Make deps"
-	$t $(MAKE) -C $(APP) deps $v
+	$i "Add the lager_transform parse_transform to ERLC_OPTS"
+	$t echo "ERLC_OPTS += +'{parse_transform, lager_transform}'" >> $(APP)/Makefile
 
-	$i "Check deps have compiled"
-	$t test -d $(APP)/deps/lager/ebin
-	$t test -d $(APP)/deps/jason/ebin
-	$t test -d $(APP)/deps/phoenix/ebin
-
-	$i "Make the app"
-	$t $(MAKE) -C $(APP) app $v
-
-	$i "Get started apps"
-	$t $(MAKE) -C $(APP) shell SHELL_OPTS="$(filter-out erl,$(ERL)) -pa $(APP)/deps/*/ebin/ $(APP)/ebin/ $(APP)/apps/*/ebin/ -eval \" \
-		{ok, Apps} = application:ensure_all_started('$(APP)'), \
-		true = lists:member(lager, Apps), \
-		true = lists:member(jason, Apps), \
-		true = lists:member(phoenix, Apps), \
-		halt()\""
-
-	$i "Check modules aren't duplicated"
-	$t $(MAKE) -C $(APP) shell SHELL_OPTS="$(filter-out erl,$(ERL)) -pa $(APP)/deps/*/ebin/ $(APP)/ebin/ $(APP)/apps/*/ebin/ -eval \" \
-		{ok, Apps} = application:ensure_all_started('$(APP)'), \
-		[begin \
-			{ok, Mods} = application:get_key(App, modules), \
-			true = lists:sort(Mods) =:= lists:usort(Mods) \
-		end || App <- Apps], \
-		halt()\""
-else
-	$i "Test depends on a System Install of Elixir, skipping."
+ifdef LEGACY
+	$i "Add Lager, Jason and Phoenix to the applications key in the .app.src file"
+	$t perl -ni.bak -e 'print;if ($$.==7) {print "\t\telixir,\n\t\tlager,\n\t\tjason,\n\t\tphoenix,\n"}' $(APP)/src/$(APP).app.src
 endif
 
-core-elixir-test-project-rel: init
-	$i "Bootstrap a new OTP library named $(APP)"
-	$t mkdir $(APP)/
-	$t cp ../erlang.mk $(APP)/
-	$t $(MAKE) -C $(APP) -f erlang.mk bootstrap $v
+	$i "Build the application"
+	$t $(MAKE) -C $(APP) $v
 
-	$i "Configure Makefile"
-	$t echo "ERLC_OPTS += +debug_info +'{parse_transform,lager_transform}'" >> $(APP)/Makefile
-	$t echo "DEPS += lager" >> $(APP)/Makefile
-	$t echo "DEPS += jason" >> $(APP)/Makefile
-	$t echo "DEPS += phoenix" >> $(APP)/Makefile
-	$t echo "dep_lager = git https://github.com/erlang-lager/lager master" >> $(APP)/Makefile
-	$t echo "dep_jason = git https://github.com/michalmuskala/jason.git master" >> $(APP)/Makefile
-	$t echo "dep_phoenix = hex 1.7.2" >> $(APP)/Makefile
-	$t echo "$$(grep -v 'include erlang.mk' $(APP)/Makefile)" > $(APP)/Makefile
-	$t echo "include erlang.mk" >> $(APP)/Makefile
+	$i "Check that all dependencies were fetched and built"
+	$t ! test -e $(APP)/deps/elixir
+	$t test -f $(APP)/deps/lager/ebin/dep_built
+	$t test -f $(APP)/deps/jason/ebin/dep_built
+	$t test -f $(APP)/deps/phoenix/ebin/dep_built
 
-	$i "Make deps"
-	$t $(MAKE) -C $(APP) deps $v
+	$i "Check that the application was compiled correctly"
+	$t $(ERL) -pa $(APP)/ebin/ -pa $(APP)/deps/*/ebin -pa $(dir $(shell elixir -e 'IO.puts(:code.lib_dir(:elixir))'))/*/ebin -eval " \
+		{ok, Apps} = application:ensure_all_started('$(APP)'), \
+		true = lists:member(lager, Apps), \
+		true = lists:member(jason, Apps), \
+		true = lists:member(phoenix, Apps), \
+		halt()"
 
-	$i "Check a release can be made"
-	$t $(MAKE) -C $(APP) bootstrap-rel
-	$t $(MAKE) -C $(APP) rel
+	$i "Check that the Jason application depends on Elixir builtins"
+	$t $(ERL) -pa $(APP)/ebin/ -pa $(APP)/deps/*/ebin -pa $(dir $(shell elixir -e 'IO.puts(:code.lib_dir(:elixir))'))/*/ebin -eval " \
+		{ok, Apps} = application:ensure_all_started(jason), \
+		true = lists:member(elixir, Apps), \
+		true = lists:member(eex, Apps), \
+		true = lists:member(logger, Apps), \
+		true = lists:member(mix, Apps), \
+		halt()"
 
 core-elixir-nif: init
-# @todo
-ifneq ($(shell which cpp >/dev/null && echo '#include "sodium.h"' | cpp -H -o /dev/null 2>&1 | head -n1 | grep -v 'No such file or directory'),)
+
 	$i "Bootstrap a new OTP library named $(APP)"
 	$t mkdir $(APP)/
 	$t cp ../erlang.mk $(APP)/
-	$t $(MAKE) -C $(APP) -f erlang.mk bootstrap $v
+	$t $(MAKE) -C $(APP) -f erlang.mk bootstrap-lib $v
 
-	$i "Configure Makefile"
-	$t echo "DEPS += libsalty2" >> $(APP)/Makefile
-	$t echo "dep_libsalty2 = git https://github.com/Ianleeclark/libsalty2.git b11e544" >> $(APP)/Makefile
-	$t echo "$$(grep -v 'include erlang.mk' $(APP)/Makefile)" > $(APP)/Makefile
-	$t echo "include erlang.mk" >> $(APP)/Makefile
+	$i "Add Libsalty2 to the list of dependencies"
+	$t perl -ni.bak -e 'print;if ($$.==1) {print "DEPS = libsalty2\ndep_libsalty2 = git https://github.com/Ianleeclark/libsalty2.git b11e544\n"}' $(APP)/Makefile
 
-	$i "Make deps"
-	$t $(MAKE) -C $(APP) deps $v
-
-	$i "Check libsalty2 has compiled"
-	$t test -f $(APP)/deps/libsalty2/ebin/libsalty2.app
-	$t test -f $(APP)/deps/libsalty2/priv/salty_nif.so
-else
-	$i "Test depends on libsodium-dev, skipping."
+ifdef LEGACY
+	$i "Add Libsalty2 to the applications key in the .app.src file"
+	$t perl -ni.bak -e 'print;if ($$.==7) {print "\t\tlibsalty2,\n"}' $(APP)/src/$(APP).app.src
 endif
+
+	$i "Build the application"
+	$t $(MAKE) -C $(APP) $v
+
+	$i "Check that the application was compiled correctly"
+	$t $(ERL) -pa $(APP)/ebin/ -pa $(APP)/deps/*/ebin -pa $(dir $(shell elixir -e 'IO.puts(:code.lib_dir(:elixir))'))/*/ebin -eval " \
+		{ok, Apps} = application:ensure_all_started('$(APP)'), \
+		true = lists:member(libsalty2, Apps), \
+		halt()"
+
+core-elixir-rel: init
+
+	$i "Bootstrap a new release named $(APP)"
+	$t mkdir $(APP)/
+	$t cp ../erlang.mk $(APP)/
+	$t $(MAKE) -C $(APP) -f erlang.mk bootstrap-lib bootstrap-rel $v
+
+	$i "Add Lager, Jason, Phoenix to the list of dependencies"
+	$t perl -ni.bak -e 'print;if ($$.==1) {print "DEPS = lager jason phoenix\ndep_lager = git https://github.com/erlang-lager/lager master\ndep_jason = git https://github.com/michalmuskala/jason.git master\ndep_phoenix = hex 1.7.2\n"}' $(APP)/Makefile
+
+	$i "Add the lager_transform parse_transform to ERLC_OPTS"
+	$t echo "ERLC_OPTS += +'{parse_transform, lager_transform}'" >> $(APP)/Makefile
+
+ifdef LEGACY
+	$i "Add Lager, Jason and Phoenix to the applications key in the .app.src file"
+	$t perl -ni.bak -e 'print;if ($$.==7) {print "\t\tlager,\n\t\tjason,\n\t\tphoenix,\n"}' $(APP)/src/$(APP).app.src
+endif
+
+	$i "Build the release"
+	$t $(MAKE) -C $(APP) $v
+
+	$i "Check that the release was built"
+	$t test -d $(APP)/_rel
+	$t test -d $(APP)/_rel/$(APP)_release
+	$t test -d $(APP)/_rel/$(APP)_release/bin
+	$t test -d $(APP)/_rel/$(APP)_release/lib
+	$t test -d $(APP)/_rel/$(APP)_release/releases
+	$t test -d $(APP)/_rel/$(APP)_release/releases/1
