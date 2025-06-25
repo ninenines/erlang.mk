88ab050b42ef57d56d140b7a2ccda902e9d3aa43
Loïc Hoguin <essen@ninenines.eu>
2025-06-25 13:01:29 +0200

    Elixir: Load LOCAL_DEPS applications before compiling
    Should make x509 and others work.

--- core/elixir.mk
+++ core/elixir.mk
@@ -182,6 +182,7 @@ endef
 define compile_ex.erl
 	{ok, _} = application:ensure_all_started(elixir),
 	{ok, _} = application:ensure_all_started(mix),
+	$(foreach dep,$(LOCAL_DEPS),ok = application:load($(dep)),)
 	ModCode = list_to_atom("Elixir.Code"),
 	ModCode:put_compiler_option(ignore_module_conflict, true),
 	ModComp = list_to_atom("Elixir.Kernel.ParallelCompiler"),
